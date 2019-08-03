6f5174eb7112540fbd2191dc358734f366c50bd1
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-03 22:43:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+function whisper(string) {
+  return string.toLowerCase;
+}
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
